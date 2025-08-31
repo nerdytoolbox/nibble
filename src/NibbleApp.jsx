9e23f6ba023351cf3767bd2bf142108bff5d1ffc
Vera Prinsen <vera.prinsen@nedap.com>
2025-08-31 12:06:44 +0200

    Go to version 1.1.0

--- src/NibbleApp.jsx
+++ src/NibbleApp.jsx
@@ -16,7 +16,7 @@ const NibbleApp = () => {
       <div className="tool-footer">
         <a href="https://github.com/nerdytoolbox/nibble/issues/new?template=ISSUE_TEMPLATE.md">Report issues / Feature requests</a>
         <span>&nbsp;|&nbsp;</span>
-        <a href="https://github.com/nerdytoolbox/nibble/releases/tag/v1.0.0">v1.0.0</a>
+        <a href="https://github.com/nerdytoolbox/nibble/releases/tag/v1.1.0">v1.1.0</a>
       </div>
     )
   }
